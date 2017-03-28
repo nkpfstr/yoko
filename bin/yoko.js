a7fb6416d5ae5e1e378d16e97163a90650700de9
Nick Pfisterer <nick@lunamachine.net>
2017-03-28 01:42:38 -0700

    Prototype for 'new' command

--- bin/yoko.js
+++ bin/yoko.js
@@ -4,6 +4,9 @@
 
 // Import dependencies
 const yoko = require('commander')
+const fs = require('fs-extra')
+const yaml = require('js-yaml')
+const log = require('lloogg')
 
 // yoko -v, --version
 yoko.version(require('../package').version)
@@ -13,9 +16,39 @@ yoko
   .command('new <path>')
   .alias('n')
   .description('Create a new site')
+  .option('-b, --blank', 'Create a new site with a blank theme')
   .action(path => {
-    // Get template files
-    // Generate a config file
+    if (path) {
+      // Initialize site config
+      let siteConfig = {
+        site: {
+          title: path,
+          url: 'http://change.me'
+        }
+      }
+      // Create site directory
+      fs.ensureDir(path, err => {
+        if (err) {
+          return log.error(err)
+        }
+
+        // Copy theme files
+        fs.copy(`${__dirname}/../themes/default`, path, err => {
+          if (err) {
+            return log.error(err)
+          }
+
+          // Write config file
+          fs.outputFile(`${path}/config.yml`, yaml.dump(siteConfig), err => {
+            if (err) {
+              return log.error(err)
+            }
+          })
+
+          log.success('Site created successfully')
+        })
+      })
+    }
   })
 
 // yoko build
